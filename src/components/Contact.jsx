7279e502cf4ced861ac9447a078f5bcfc3387ca5
ticoniq_ <okolietochi@gmail>
2023-08-08 11:37:53 +0100

    updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,24 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
 import { PhoneIcon, EnvelopeIcon } from "@heroicons/react/24/outline";
 
 function Contact() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = (e) => {
+    if (!name && !email && !message) {
+      e.preventDefault();
+      setError('All fields are required');
+      setTimeout(() => {
+        setError('');
+      }, 2000);
+      return;
+    }
+  };
+  
   return (
     <section id="Contact" className="bg-newbBlue dark:">
       <div className="max-w-7xl mx-auto px-5 py-20">
@@ -48,45 +66,37 @@ function Contact() {
               <h2 className="w-72 mb-6 text-xl font-bold text-white">
                 Contact me, let’s make magic together
               </h2>
-              <form>
+              <form action="https://formspree.io/f/xrgwpgzk" onSubmit={handleSubmit} method="post">
                 <div className="relative mb-6" data-te-input-wrapper-init>
                   <input
                     type="text"
-                    className="peer block min-h-[auto] w-full rounded-lg border-0 bg-white py-2 px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                    name="name"
+                    className="peer block min-h-[auto] w-full rounded-lg border-0 bg-white py-2 px-3 leading-[1.6] outline-none"
                     id="exampleInput90"
                     placeholder="Name"
                   />
-                  <label
-                    className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-newGray transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none ">
-                    Name
-                  </label>
                 </div>
                 <div className="relative mb-6" data-te-input-wrapper-init>
                   <input
                     type="email"
-                    className="peer block min-h-[auto] w-full rounded-lg border-0 bg-white py-2 px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none  [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                    name="email"
+                    className="peer block min-h-[auto] w-full rounded-lg border-0 bg-white py-2 px-3 leading-[1.6] outline-none"
                     id="exampleInput91"
                     placeholder="Email address"
                   />
-                  <label
-                    className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-newGray transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:peer-focus:text-primary">
-                    Email address
-                  </label>
                 </div>
                 <div className="relative mb-6" data-te-input-wrapper-init>
                   <textarea
-                    className="peer block min-h-[auto] w-full rounded-lg border-0 bg-white py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                    className="peer block min-h-[auto] w-full rounded-lg border-0 bg-white py-[0.32rem] px-3 leading-[1.6] outline-none"
                     id="exampleFormControlTextarea1"
+                    name="message"
                     rows="5"
                     placeholder="Your message"></textarea>
-                  <label
-                    className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[100%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-newGray transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none">
-                    Message
-                  </label>
+                    {error && <p className="messages mt-3 text-red-600">{error}</p>}
                 </div>
                 <button
                   type="submit"
-                  className="p-3 px-6 text-white font-bold bg-newYellow rounded-md hover:bg-transparent hover:text-white hover:border-2 hover:border-newYellow">
+                  className="btn float-right border-0 p-3 px-6 text-white font-bold bg-newYellow rounded-md hover:bg-transparent hover:text-white hover:border-2 hover:border-newYellow">
                   Email me
                 </button>
               </form>
