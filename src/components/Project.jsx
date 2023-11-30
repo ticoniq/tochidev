a6d129bf32ff180a494863d3d88a7be45e82a612
ticoniq_ <okolietochi@gmail>
2023-11-30 13:51:00 +0100

    Updated

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -59,7 +59,7 @@ function Project() {
     },
     {
       id: 5,
-      title: "Weather JS 4",
+      title: "Weather JS",
       image: weather,
       details:
         "weather web application made using HTML5, CSS3, and JavaScript. It's working process is simple. It takes the name of the City as input from the -user and create a HTTP request and send it to Openweathermap.org API to get back the deatils in the form of a JSON Response.",
