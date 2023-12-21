9c2375049bcde04295e58f83477a910ac2b8c56b
ticoniq_ <okolietochi@gmail>
2023-12-21 14:25:45 +0100

    Add @vercel/speed-insights package

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Blog from "./components/Blog";
 import Contact from "./components/Contact";
 import Footer from "./components/layouts/Footer";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App({ children }) {
   const [darkMode, setDarkMode] = useState(() => {
@@ -28,6 +29,7 @@ function App({ children }) {
   return (
     <div className={`${darkMode ? "dark" : ""} font-Poppins`}>
       {children}
+      <SpeedInsights />
       <Analytics />
       <Navbar dark={handleDarkMode} data={darkMode} />
       <main>
